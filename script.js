276c39bd48bc66770a46acf712842bda24608142
nicholasdouglas <158283953+nicholasdouglas@users.noreply.github.com>
2024-02-05 09:51:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+alert("penis");
+function somar(){
+    console.log ("AMOGUS");
+    var num1  = document.getElementById("num01").valueAsNumber;
+    var num2 =  document.getElementById("num02").valueAsNumber;
+    var resu = num1 + num2;
+    console.log(num1);
+    console.log(num2);
+    console.log(resu);
+}
