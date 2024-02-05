4caffc989d30f61692af50da9764d9d6c83450fb
nicholasdouglas <158283953+nicholasdouglas@users.noreply.github.com>
2024-02-05 12:26:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 alert("penis");
-function somar(){
-    console.log ("AMOGUS");
+function somar()
+{
     var num1  = document.getElementById("num01").valueAsNumber;
     var num2 =  document.getElementById("num02").valueAsNumber;
-    var resu = num1 + num2;
-    console.log(num1);
-    console.log(num2);
-    console.log(resu);
+    var resultado = num1 + num2;
+    document.getElementById("resultadoSoma").textContent = resultado;
+}
+
+function subtrair()
+{
+    var num1  = document.getElementById("num03").valueAsNumber;
+    var num2 =  document.getElementById("num04").valueAsNumber;
+    var resultado = num1 - num2;
+    document.getElementById("resultadoSub").textContent = resultado;
+}
+
+function multiplicar()
+{
+    var num1  = document.getElementById("num05").valueAsNumber;
+    var num2 =  document.getElementById("num06").valueAsNumber;
+    var resultado = num1 * num2;
+    document.getElementById("resultadoMult").textContent = resultado;
+}
+
+function dividir()
+{
+    var num1  = document.getElementById("num07").valueAsNumber;
+    var num2 =  document.getElementById("num08").valueAsNumber;
+    var resultado = num1 / num2;
+    document.getElementById("resultadoDiv").textContent = resultado;
 }
